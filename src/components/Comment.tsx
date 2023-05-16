0f2d1a3a7501c2df18abc58e546027f062f71472
sam-yng <young142001@hotmail.com>
2023-05-16 11:00:50 +1000

    moving on

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -9,6 +9,8 @@ export type CommentProps = {
   content: string;
   score?: number;
   avatar?: string;
+  replies?: Array<CommentProps>;
+  classNames?: string;
 };
 
 const Comment: React.FC<CommentProps> = ({
@@ -17,6 +19,7 @@ const Comment: React.FC<CommentProps> = ({
   content,
   score,
   avatar,
+  classNames,
 }: CommentProps) => {
   const { comments } = useComment();
 
@@ -25,21 +28,26 @@ const Comment: React.FC<CommentProps> = ({
   }
 
   return (
-    <article className="bg-white w-[50%] flex flex-row rounded-lg m-auto px-6 py-10 mb-8">
-      <div className="flex flex-col justify-between items-center mr-5 h-[6.5rem] bg-[#F5F6FA] rounded-lg w-[6rem]">
-        <img className="p-3" src={plus} />
-        <h2 className="font-rmedium text-indigo-900">{score}</h2>
-        <img className="p-3" src={minus} />
-      </div>
-      <div>
-        <div className="flex flex-row items-center space-x-4">
-          <img className="h-8" src={avatar} />
-          <h1 className="font-rbold">{username}</h1>
-          <h1 className="text-[#585858] font-rregular">{createdAt}</h1>
+    <main className="justify-center flex">
+      <article
+        className={`${classNames} bg-white w-[50%] mb-10 p-6 flex flex-row rounded-lg`}
+      >
+        <div className="flex flex-col justify-between items-center mr-5 h-[6.5rem] bg-[#F5F6FA] rounded-lg w-[6rem]">
+          <img className="p-3" src={plus} />
+          <h2 className="font-rmedium text-indigo-900">{score}</h2>
+          <img className="p-3" src={minus} />
         </div>
-        <p className="font-rregular mt-4 text-[#727272]">{content}</p>
-      </div>
-    </article>
+        <div>
+          <div className="flex flex-row items-center space-x-4">
+            <img className="h-8" src={avatar} />
+            <h1 className="font-rbold">{username}</h1>
+            <h1 className="text-[#585858] font-rregular">{createdAt}</h1>
+            <h1 className="text-indigo-900 font-rmedium">Reply</h1>
+          </div>
+          <p className="font-rregular mt-4 text-[#727272]">{content}</p>
+        </div>
+      </article>
+    </main>
   );
 };
 
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,7 +5,6 @@ import amyrobson from "../assets/images/avatars/image-amyrobson.png";
 import maxblagun from "../assets/images/avatars/image-maxblagun.png";
 import ramsesmiron from "../assets/images/avatars/image-ramsesmiron.png";
 import juliusomo from "../assets/images/avatars/image-juliusomo.png";
-import Reply from "./Reply";
 
 const images = [amyrobson, maxblagun, ramsesmiron, juliusomo];
 
@@ -17,7 +16,7 @@ const Main: React.FC = () => {
   }
 
   return (
-    <main className="py-16 ">
+    <main className="py-16">
       {comments.comments.map((item) => (
         <Comment
           score={item.score}
@@ -28,16 +27,21 @@ const Main: React.FC = () => {
           content={item.content}
         />
       ))}
-      {comments.comments[1].replies.map((item) => (
-        <Reply
-          avatar={images[item.id - 1]}
-          key={item.id}
-          score={item.score}
-          username={item.user.username}
-          createdAt={item.createdAt}
-          content={item.content}
-        />
-      ))}
+      {comments.comments
+        .filter((item) => item.replies.length > 0)
+        .map((item) =>
+          item.replies.map((reply) => (
+            <Comment
+              score={reply.score}
+              avatar={images[reply.id - 1]}
+              key={reply.id}
+              username={reply.user.username}
+              createdAt={reply.createdAt}
+              content={reply.content}
+              classNames="w-[44%]"
+            />
+          ))
+        )}
     </main>
   );
 };
